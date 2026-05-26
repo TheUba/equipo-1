d55c2ad17afc27711bfa7c4dd264b8e5e4d40ead
LuisLeon06 <lleon8039@gmail.com>
2026-05-26 14:21:59 +0000

    feat: agrega bitácora de películas y series

---
+++ notebooks/bitacora-de-peliculas-y-series.py
@@ -0,0 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Este notebook va a tener una lista de peliculas y series
