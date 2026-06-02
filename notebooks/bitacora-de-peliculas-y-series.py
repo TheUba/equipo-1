d88429f133416dcee1f5f465dc4da9b179cebd7a
uba <uba.aramayo@gmail.com>
2026-06-02 10:54:08 -0300

    feat: agrega pelicula Tonto y retonto

--- notebooks/bitacora-de-peliculas-y-series.py
+++ notebooks/bitacora-de-peliculas-y-series.py
@@ -15,3 +15,12 @@
 
 # %% [markdown]
 # # Este notebook va a tener una lista de peliculas y series
+
+# %% [markdown]
+# ## Tonto y retonto
+#
+# - Tipo: pelicula
+# - Género: comedia
+# - Año de estreno: no se
+# - Reseña: dos amigos que viajan.
+#
